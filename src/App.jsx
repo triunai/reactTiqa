be92bc65571f2a03a1c6ea08bef088928b696302
Khumeren R <khucode@gmail.com>
2024-01-04 12:34:28 +0800

    first commit before lunch

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function Header(){
+  return (
+      <header>
+        <img src="src/assets/etiqa.svg" alt="Stylized atom" />
+        <h1>E T I Q A N </h1>
+        <p>
+          What does it mean to be an ETIQAN? 🤔💭
+        </p>
+      </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <h2>Lets find out !</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
